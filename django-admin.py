392c3eaf7f022698b561c6c00fde3f96e47ce71b
Apurv Paliwal <apurv_paliwal@Apurvs-MacBook-Pro.local>
2021-10-24 14:25:44 +0530

    My Django blog app

---
+++ django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/apurv_paliwal/Desktop/django_projects/django_tut/djng_tut/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
